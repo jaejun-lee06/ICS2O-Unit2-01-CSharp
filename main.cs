196501fcc9070fec3df5dc863ee7ffc50a29d735
jaejun-lee06 <99355321+jaejun-lee06@users.noreply.github.com>
2022-03-10 03:00:06 +0000

    change

--- main.cs
+++ main.cs
@@ -1,7 +1,15 @@
+// Created by: Jaejun Lee
+// Created on: Mar 2022
+//
+// This program displays, "Hello, World!"
+
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("\nDone.");
+    }
 }
